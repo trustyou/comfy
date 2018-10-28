b51ab2a33985b9395c266599b9e01601e0210e60
David Suilea <david.suilea@gmail.com>
2018-10-28 23:36:11 +0100

    Update example with failing test data

--- examples/example.py
+++ examples/example.py
@@ -1,26 +1,31 @@
-from configparser import ConfigParser
+try:
+    from configparser import ConfigParser, NoOptionError
+except ImportError:
+    # Python 2
+    from ConfigParser import ConfigParser, NoOptionError
 
 from comfy import *
 
 
 class MyConfig(Schema):
 
-    class Server(Section):
-        host = Option()
-        port = IntOption()
-    server = None  # type: Server
+    class TmpDirectories(Section):
+        img_tmp = Option()
+        vid_tmp = Option()
+        max_files = IntOption()
+    tmp_directories = None
 
 
 if __name__ == "__main__":
 
     config_parser = ConfigParser()
-    config_parser.read_string("""
-[server]
-host=api.trustyou.com
-port=8080
+
+    config_parser.read_string(u"""
+[tmp_directories]
+img_tmp=/tmp/img
+vid_tmp=/tmp/vid
+max_files=1024
 """)
     config = MyConfig(config_parser)
 
-    print("Host:", config.server.host)
-    print("Port:", config.server.port)
-
+    print(config.tmp_directories.img_tmp)
